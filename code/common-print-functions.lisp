e7548dffeb285b39fa50f45a12a478b421cbfb94
Arthur Miller <arthur.miller@live.com>
2025-05-23 12:33:07 +0200

    fix length bug in generic print

--- code/common-print-functions.lisp
+++ code/common-print-functions.lisp
@@ -6,7 +6,7 @@
             (if (pretty-print directive)
                 (princ (directive-argument directive) s)
                 (prin1 (directive-argument directive) s)))))
-    (if (> (argument-precision directive) 0)
+    (if (> (length string) (argument-precision directive) 0)
         (subseq string 0 (argument-precision directive))
         string)))
 
